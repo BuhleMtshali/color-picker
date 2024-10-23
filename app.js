1aab517ea63e8257ea6675148ef8bcebe636beb6
Buhle <mtshalizanobuhle7@gmail.com>
2024-10-24 01:06:47 +0200

    updated the stylesheet

--- app.js
+++ app.js
@@ -6,5 +6,21 @@ let pallatesElement = document.querySelectorAll('.pallate')
 document.getElementById('button').addEventListener('click', (e) => {
     e.preventDefault();
     let colorValue = inputElement.value.slice(1)
-    console.log(colorValue)
+    findColor(colorValue)
 })
+
+//function for the api function
+
+function findColor(value){
+    let apiUrl = `https://www.thecolorapi.com/scheme?hex=${value}`
+    axios.get(apiUrl).then(renderColor)
+}
+
+//function for rendering out the data
+
+function renderColor(response){
+    console.log(response.data)
+    pallatesElement.forEach((pallate, index) => {
+        pallate.style.backgroundColor = response.data.colors[index].hex.value
+    })
+}
