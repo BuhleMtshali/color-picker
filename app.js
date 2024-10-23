4afc34dc9d6b8b6d890364346a12f89b1fb21ee8
Buhle <mtshalizanobuhle7@gmail.com>
2024-10-24 00:51:41 +0200

    updated the app.js file

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // defining elements
 let inputElement = document.getElementById('color-input');
+let pallatesElement = document.querySelectorAll('.pallate')
+
+//event listener
+document.getElementById('button').addEventListener('click', (e) => {
+    e.preventDefault();
+    let colorValue = inputElement.value.slice(1)
+    console.log(colorValue)
+})
