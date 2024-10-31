63d39154664011502ead4e4f7477ed4918691052
Buhle <mtshalizanobuhle7@gmail.com>
2024-11-01 00:10:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 // defining elements
-let inputElement = document.getElementById('color-input');
-let pallatesElement = document.querySelectorAll('.pallate')
-
+let inputElement = document.getElementById("color-input");
+let pallatesElement = document.querySelectorAll(".pallate");
 
 //event listener
-document.getElementById('button').addEventListener('click', (e) => {
-    e.preventDefault();
-    let colorValue = inputElement.value.slice(1)
-    findColor(colorValue)
-})
+document.getElementById("button").addEventListener("click", (e) => {
+  e.preventDefault();
+  let colorValue = inputElement.value.slice(1);
+  findColor(colorValue);
+});
 
 //function for the api function
 
-function findColor(value){
-    let apiUrl = `https://www.thecolorapi.com/scheme?hex=${value}`
-    axios.get(apiUrl).then(renderColor)
+function findColor(value) {
+  let apiUrl = `https://www.thecolorapi.com/scheme?hex=${value}`;
+  axios.get(apiUrl).then(renderColor);
 }
 
 //function for rendering out the data
 
-function renderColor(response){
-    console.log(response.data)
-    pallatesElement.forEach((pallate, index) => {
-        const colorHex = response.data.colors[index].hex.value;
-        pallate.style.backgroundColor = response.data.colors[index].hex.value
-      pallate.innerHTML = response.data.colors[index].hex.value
-      pallate.style.color = 'white'
-      pallate.style.fontWeight = 'bold'
-let body = document.getElementById('body')
-body.style.backgroundColor = response.data.colors[index].hex.value
+function renderColor(response) {
+  console.log(response.data);
+  pallatesElement.forEach((pallate, index) => {
+    const colorHex = response.data.colors[index].hex.value;
+    pallate.style.backgroundColor = response.data.colors[index].hex.value;
+    pallate.innerHTML = response.data.colors[index].hex.value;
+    pallate.style.color = "white";
+    pallate.style.fontWeight = "bold";
+    let body = document.getElementById("body");
+    body.style.backgroundColor = response.data.colors[index].hex.value;
 
-      //adding copy to clipboard function
-pallate.addEventListener('click', () => {
-    navigator.clipboard.writeText(colorHex)
-    .then(() => {
-        alert(`Copied ${colorHex} to Clipboard!`)
-    })
-    .catch(err => {
-        console.error('Failed to copy text: ', err)
-    })
-})
-     
-    })
+    //adding copy to clipboard function
+    pallate.addEventListener("click", () => {
+      navigator.clipboard
+        .writeText(colorHex)
+        .then(() => {
+          alert(`Copied ${colorHex} to Clipboard!`);
+        })
+        .catch((err) => {
+          console.error("Failed to copy text: ", err);
+        });
+    });
+  });
 }
